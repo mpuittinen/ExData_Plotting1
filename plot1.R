0117fc6488ed4bb508dae0f6c7888f09b3b78fac
Mikael Puittinen <mikael@iki.fi>
2015-10-11 10:33:45 +0300

    Update implementation of plot1

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 fullData <- read.delim("household_power_consumption.txt",sep=";", stringsAsFactors=FALSE)
-rowDates <- as.Date(strptime(fullData[,"Date"], "%d/%m/%Y"))
-plotData <- fullData[rowDates == as.Date("2007-02-02") | rowDates == as.Date("2007-02-01"),]
+fullData$dateTime <- strptime(paste(fullData$Date, fullData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+plotData <- fullData[as.Date(fullData$dateTime) == as.Date("2007-02-02") | as.Date(fullData$dateTime) == as.Date("2007-02-01"),]
 
 hist(as.numeric(plotData$Global_active_power), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.copy(png, "plot1.png")
+dev.copy(png, "plot1.png", width=480, height=480, units="px")
 dev.off()
